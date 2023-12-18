ab53a384a99036dad04611e4f10ed76b194aa059
Onur Umutluoğlu <onurumutluoglu@gmail.com>
2023-12-18 05:06:33 +0300

    Add project files.

---
+++ service/Order/Ordering.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Commands.OrderCreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderCreateCommand>().ReverseMap();
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
